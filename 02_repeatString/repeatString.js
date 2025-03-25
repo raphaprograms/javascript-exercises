e634a287c52651ad4537bf740c7ba2a525e1d791
raphaprograms <203123602+raphaprograms@users.noreply.github.com>
2025-03-25 16:09:57 -0700

    Complete javascript-exercises 02 repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num <0) {
+        return 'ERROR'
+    }
+    let out = '';
+    for (let i = 0; i < num; i++)
+    out += string;
+    return out
 
 };
 
