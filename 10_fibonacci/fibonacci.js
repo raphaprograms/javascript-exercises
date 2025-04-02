2bd5f36bfa5848d15c9ddbcef00b6f9f1db3bad8
raphaprograms <203123602+raphaprograms@users.noreply.github.com>
2025-04-02 12:28:26 -0700

    complete 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let seqnum 
+    if (typeof n !== 'number'){
+        seqnum = parseInt(n);
+    } else {
+        seqnum = n
+    }
 
+    if (seqnum < 0) return "OOPS";
+    if (seqnum == 0) return 0;
+
+    let firstPrev = 1;
+    let secondPrev = 0;
+
+    for (let i = 2; i <= seqnum; i++) {
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+
+    return firstPrev;
 };
 
 // Do not edit below this line
