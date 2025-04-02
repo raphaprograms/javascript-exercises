e78dd98b77729473648fb11a11882aef03d675d2
raphaprograms <203123602+raphaprograms@users.noreply.github.com>
2025-04-02 12:36:01 -0700

    Complete 11_getTheTitles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,15 @@
-const getTheTitles = function() {
-
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+const getTheTitles = function(array) {
+    return array.map(item => item.title)
 };
 
 // Do not edit below this line
