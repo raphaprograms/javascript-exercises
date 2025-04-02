3a18eb61c1cbf295ed633e755c2a6e430b0e8968
raphaprograms <203123602+raphaprograms@users.noreply.github.com>
2025-04-02 11:31:18 -0700

    Complete 08_calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce(((prev, current) => prev + current), 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce(((prev, current) => prev * current), 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let total = 1;
+    for(i = a; i > 0; i--) {
+      total *= i; 
+    }
+    return total;
 };
 
 // Do not edit below this line
